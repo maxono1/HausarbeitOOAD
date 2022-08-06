93ede607ee27acf91cd88ef6732328a6909df64e
maxono <maximilian.jaesch@hs-osnabrueck.de>
2022-08-06 11:17:03 +0200

    quelle geaddet interface

--- src/main/java/com/hausarbeitooad/model/SceneName.java
+++ src/main/java/com/hausarbeitooad/model/SceneName.java
@@ -1,11 +1,13 @@
 package com.hausarbeitooad.model;
 
 /**
- *  Hilfe für Scenen wechseln
+ * Hilfe für Scenen wechseln
  *
  * @author 1st: Abdurrahman Azattemür, 2nd:Tim Cirksena
- * @source: Selbst erstellt
- * */
+ * idee von:
+ * @source: https://github.com/ksnortum/javafx-multi-scene-fxml/tree/pre-javafx-11
+ */
 public enum SceneName {
-    MAIN, GUTHABENVERWALTEN, GUTHABENAUFLADEN, GAME_DETAIL_VIEW, REVIEW_VIEW, LOGIN, SHOP_MENU, COLLECTION_VIEW, SHOP_ITEM, OPTION_VIEW, REZENSION_VIEW;
+    MAIN, GUTHABENVERWALTEN, GUTHABENAUFLADEN, GAME_DETAIL_VIEW,
+    REVIEW_VIEW, LOGIN, SHOP_MENU, COLLECTION_VIEW, SHOP_ITEM, OPTION_VIEW, REZENSION_VIEW
 }
--- src/main/java/com/hausarbeitooad/model/Stageable.java
+++ src/main/java/com/hausarbeitooad/model/Stageable.java
@@ -3,12 +3,11 @@ package com.hausarbeitooad.model;
 import javafx.stage.Stage;
 
 /**
- *  Dient als Hilfe zum Wechseln von Scenes
+ * Dient als Hilfe zum Wechseln von Scenes
  *
- * @author 1st: Maximilian Jaesch, 2nd:Tim Cirksena
- * @source: Selbst erstellt
- * */
+ * @author Tim Cirksena
+ * @source: <a href="https://github.com/ksnortum/javafx-multi-scene-fxml/tree/pre-javafx-11">...</a>
+ */
 public interface Stageable {
-
     void setStage(Stage stage);
 }
